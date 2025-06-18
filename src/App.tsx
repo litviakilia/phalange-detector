34b2f188f1ee23aea761c98a77113aa3791d9314
litviakilia <litviakilia@gmail.com>
2025-06-18 19:25:29 +0100

    fix: use official Google Cloud Storage URL for hand_landmarker.task model file

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
         log('FilesetResolver loaded')
         const landmarker = await HandLandmarker.createFromOptions(vision, {
           baseOptions: {
-            modelAssetPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.12/wasm/hand_landmarker.task',
+            modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task',
             delegate: 'GPU'
           },
           runningMode: 'VIDEO',
